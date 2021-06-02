1e38d83d606e4b6163982412d8f467eb2750c997
Deepak_Arora <Deepakarora0145@gmail.com>
2021-06-02 22:49:03 +0530

    Initial commit

---
+++ src/main/java/com/masco/MascoGitBasics/MascoGitBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.masco.MascoGitBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascoGitBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascoGitBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masco/MascoGitBasics/MascoGitBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masco.MascoGitBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascoGitBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
